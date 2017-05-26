11c2f7017da73240a85f0f9a4fc4d32abceae56b
Carlospardo14 <carlospardo1411@gmail.com>
2017-05-26 09:17:03 -0500

    Primera version metodos ordenamiento

---
+++ build/CMakeFiles/ordenamiento.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/carlos/Ordenamiento/src/main.cpp" "/home/carlos/Ordenamiento/build/CMakeFiles/ordenamiento.dir/src/main.cpp.o"
+  "/home/carlos/Ordenamiento/src/ordenamiento.cpp" "/home/carlos/Ordenamiento/build/CMakeFiles/ordenamiento.dir/src/ordenamiento.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ordenamiento.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ordenamiento.dir/src/ordenamiento.cpp.o"
+  "CMakeFiles/ordenamiento.dir/src/main.cpp.o"
+  "ordenamiento.pdb"
+  "ordenamiento"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ordenamiento.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
